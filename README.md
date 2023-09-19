9da5164a830290bc54bf12c5754a55dc06ddc3b3
Jag Johal <jagjohal@Jags-MacBook-Air.local>
2023-09-19 23:08:23 +0100

    update to js and readme

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Weather-Dashboard
-Server API - 5 Day Weather Forecast
+Server API - Weather Forecast
+
+## Description 
+
+What? This is a third-party web API for a weather forecast on any city entered.
+
+Why? To apply the JS query techniques within WEB APIs and link all 3 program languages including js / css / html
+
+How? incuding buttons, calendar view with timing and use of js query for function 
+
+[Click link for Weather Dashboard page index.html](index.html)
+
+## Deployed Webpage Link
+
+https://kapil319.github.io/Weather-Dashboard/?
+
+
+
+## Installation
+
+Create GitHub respository 
+Open starter code in VS Code
+Use Javascript / Jquery  logic to provide user inputs for weather dashboard API and link with html / css.
+
+
+
+## Credits
+
+Andrew Hoang,
+Jamie Lujan,
+Nazeh Abel
+
+Module 8 Challenge:
+https://zoom.us/rec/play/LaJPmXQ9eb6AXO4mskpRvOnbLkuU00GQA2FlOuqMsv5WQKfKK7a_ihsu0iFy13TP9dFCgBFpbkCHNese.0VA3ZI4SYthaQX_e?canPlayFromShare=true&from=share_recording_detail&continueMode=true&componentName=rec-play&originRequestUrl=https%3A%2F%2Fzoom.us%2Frec%2Fshare%2FpkVApsOwkre39W-cpn3biyC255QIH1Z1OdOanAaCYOZ20d9eFt-vruqH-ksmt5wX.GXmnixxE60sNxUXo
+
+## License
+
+License MIT
+
+## Features
+
+js query functions, save buttons , local storage items and fetch API data
+
+
+*END OF README*
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,95 @@ let searchHistory = []
 
 let searchInput = $("#search-input");
 let searchForm = $("#search-form");
-let searchHistoryContainer = $("#history")
+let searchHistoryContainer = $("#history");
+let todayContainer = $("#today");
+
+function renderSearchHistory() {
+    searchHistoryContainer.html("");
+    
+    for (let i = 0; i < searchHistory.length; i++) {
+    let btn = $("<button>");
+    btn.attr("type", "button ");
+    btn.addClass("history-btn btn-history");
+    btn.text(searchHistory[i]);
+    btn.attr("data-search", searchHistory[i]);
+    searchHistoryContainer.append(btn);     
+    }
+}
+
+function appendSearchHistory(){
+    if(searchHistory.indexOf(search) ! == -1) {
+        return
+        }
+
+        searchHistory.push(search);
+
+        localStorage.setItem("search-history", JSON.stringify(searchHistory));
+        renderSearchHistory()
+}
+
+function renderCurrentWeather(city, weatherData) {
+    let date = moment().format("DD/MM/YYYY");
+    let tempC = weatherData["main"]["temp"];
+    let windKph = weatherData["wind"]["speed"];
+    let humidity = weatherData["main"]["humidity"];
+
+    // let iconURL = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png;
+
+    let iconDescription = weatherData.weather[0].description || weather
+
+    let card = $("<div>")
+    let cardBody = $("<div>")
+    let weatherIcon = $("<img>")
+    let heading = $("<h2>")
+
+    let tempEl = $("<p>")
+    let windEl = $("<p>")
+
+    let humidityEl = $("<p>")
+
+    card.attr("class", "card");
+
+    cardBody.attr("class", 'card-body");
+
+    card.append(cardBody);
+
+    heading.attr("class", "h3 card-title");
+    tempEl.attr("class", "card-text");
+    windEl.attr("class", "card-text");
+    humidityEl.attr("class", "card-text");
+
+    heading.text(`${city} (${date})`);
+    weatherIcon.attr("src", iconURL);
+    weatherIcon.attr("alt", iconDescription);
+
+    heading.append(weatherIcon);
+    tempEl.text(`Temp ${tempC} C`);
+    windEl.text(`Wind ${windKPH} KPH`);
+    humidityEl.text(`Humidity ${humidity} %`);
+    cardBody.append(heading, tempEl, windEl, humidityEl);
+    
+    todayContainer.html("");
+    todayContainer.append(card);
+
+}
+
+function fetchWeather(location) {
+    let lattitude = location.lat; 
+    let longitude = location.lon; 
+    let city = location.name;
+
+    // let queryWeatherURL = `${weatherAPIURL}/data/2.5/forecast?lat=${lattitude}&lon=${longitude}&units=metric&appid=${weatherAPIKey}
+
+    $.ajax({
+    url: queryWeatherURL
+    method: "GET"
+    }).then(function(response){
+        render.CurrentWeather(city, response.list[0]);
+        render.Forecast(data.list);
+
+    })
+}
 
 function fetchCoord(search) {
 
@@ -22,34 +110,29 @@ function fetchCoord(search) {
                 if(!response[0]) {
                     alert("location not found")
                 }  else {
-                        if(searchHistory.indexOf(search) ! == -1) {
-                        return
-                        }
-
-                        searchHistory.push(search);
-
-                        localStorage.setItem("search-history", JSON.stringify(searchHistory));
-    
-                        searchHistoryContainer.html("")
-    
-                        for let i = 0; i < searchHistory.length; i++){
-                        let btn = $("<button>");
-                        btn.attr("type", "button ")
-                        btn.addClass("history-btn btn-history")
-                        btn.text(searchHistory[i])
-                        btn.attr("data-search", searchHistory[i]) 
-                        searchHistoryContainer.append(btn)
+                    appendSearchHistory(search)  
+                    fetchWeather(response[0])
 
                     } 
                 }   
-            })
+    })
   
+
+function initialiseSearchHistory(){
+    let storedHistory = localStorage.getItem("search-history");
+    if(storedHistory) {
+        searchHistory = JSON.parse(storedHistory);
+
+    }
+}
 
 function submitSearchFrom(event) {
     event.preventDefault();                                                  
 
     let search = searchInput.val().trim();
     fetchCoord(search);
+    searchInput.val("");
 }
 
+initialiseHistory()
 searchForm.on("submit, submitSearchForm");    
